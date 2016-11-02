18e0afa681d9b3f033ed013e415acaa0aa43b357
chdonncha <chdonncha@gmail.com>
2016-11-02 18:00:07 +0000

    complete routing episode

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,22 @@ Route::get('hello/{name}', function($id) {
 });
 
 Route::post('test', function(){
-
+    
 });
 
 Route::get('test', function(){
-    echo '<form action="test" method="Post">';
+    echo '<form action="test" method="POST">';
     echo '<input type="submit">';
     // Send csrf token through, if it's not sent then an error will be thrown
     echo '<input type="hidden" value="' . csrf_token() . '" name="_token">';
+    echo '<input type="hidden" name="_method" value="DELETE">';
     echo '</form>';
 });
 
 Route::put('test', function(){
-
+    echo 'We have just updated an item';
 });
 
 Route::delete('test', function(){
-
+    echo 'We have just deleted an item';
 });
